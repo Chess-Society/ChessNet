84998a3a3926368c8d40c7b949210168944d3629
Andres-L1 <andreslgumuzio@gmail.com>
2025-12-24 10:12:43 +0100

    fix: restore handleConfirmAction in lesson planner

--- src/routes/panel/agenda/+page.svelte
+++ src/routes/panel/agenda/+page.svelte
@@ -198,6 +198,11 @@
         };
         showConfirmModal = true;
     }
+
+    function handleConfirmAction() {
+        if (confirmAction) confirmAction();
+        showConfirmModal = false;
+    }
 </script>
 
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-12 pt-8">
