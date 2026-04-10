d7daf3d987ec0807d91fc4e402c6d148b54cfac8
Andres-L1 <andreslgumuzio@gmail.com>
2026-04-10 14:42:11 +0200

    Updates

--- _app/immutable/chunks/BAZxH5xq.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1sscveg?.base??"/ChessNet",e=globalThis.__sveltekit_1sscveg?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/Dk78a0M0.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1al8as1?.base??"/ChessNet",a=globalThis.__sveltekit_1al8as1?.assets??s??"";export{a,s as b};
