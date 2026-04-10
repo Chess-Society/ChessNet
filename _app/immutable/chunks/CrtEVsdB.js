b62cfcc7bbd4f0ea3f954dc797de568c80fdb358
Andres-L1 <andreslgumuzio@gmail.com>
2026-04-10 14:32:53 +0200

    Updates

---
+++ _app/immutable/chunks/CrtEVsdB.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bygqj?.base??"/ChessNet",e=globalThis.__sveltekit_bygqj?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/entry/start.DJPBe5Yc.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/B1Ig0TRT.js";export{o as load_css,r as start};
