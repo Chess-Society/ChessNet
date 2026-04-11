29b2fa14445b10cd12ee93ec71dd94287703191a
Andres-L1 <andreslgumuzio@gmail.com>
2026-04-12 00:29:40 +0200

    fix(prod): remove build-blocking hooks.server.ts to restore production stability

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,7 +4,7 @@ declare global {
   namespace App {
     // interface Error {}
     interface Locals {
-      user: import("$lib/types").User | null;
+      user: any | null;
     }
     // interface PageData {}
     // interface PageState {}
